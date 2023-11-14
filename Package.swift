94b37d08ed3bc8a1bfbced8a371a50835ce544c5
Andrii Antoniak <andrii.antoniak@intellias.com>
2023-11-14 02:54:46 +0200

    Move SPM manifest file to the root folder

--- Package.swift
+++ Package.swift
@@ -18,9 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WSNetworking"),
+            name: "WSNetworking",
+            path: "WSNetworking/Sources/WSNetworking"),
         .testTarget(
             name: "WSNetworkingTests",
-            dependencies: ["WSNetworking"]),
+            dependencies: ["WSNetworking"],
+            path: "WSNetworking/Tests/WSNetworkingTests"),
     ]
 )
